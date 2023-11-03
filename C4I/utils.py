fedbf16f02d3b25d9c329c93a585384901aa5a02
Sonanchalant <78246977+SonanZhang@users.noreply.github.com>
2023-11-03 17:13:43 +0800

    Update utils.py

--- C4I/utils.py
+++ C4I/utils.py
@@ -256,5 +256,5 @@ def PooledEnsembleRegressions(Y, X, n_windows):
     params.append(mdl_params)
     #Return mean values
     params = mean(params, axis=0)
-    params = params.reshape(3,1)
+    params = params.reshape(mdl_params.shape[1],1)
     return params
